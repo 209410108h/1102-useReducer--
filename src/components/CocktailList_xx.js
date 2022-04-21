8263af4420256e195c2e48f94d8cefdaab41b5ef
htchung <htchung@gms.tku.edu.tw>
2022-04-21 21:00:18 +0800

    w09-P4 Show Cocktails with your own searchTerm

--- src/components/CocktailList_xx.js
+++ src/components/CocktailList_xx.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import Cocktail from './Cocktail_xx'
-import Loading from './Loading_xx'
+import Loading_xx from './Loading_xx'
 import { useGlobalContext } from '../context'
+import Cocktail_xx from './Cocktail_xx'
 
 const CocktailList_xx = () => {
+  const { loading, cocktails } = useGlobalContext();
+  if(loading){
+    return <Loading_xx />
+  }
+
+  if(cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {
+          cocktails.map( (item) => {
+            return <Cocktail_xx key={item.id} {...item} />
+          })
+        }
+      </div>
+    </section>
   )
 }
 
